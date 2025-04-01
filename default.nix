bcd8430ee8ef98cf8c94a75e995a7107bde9cb97
sadan <117494111+sadan4@users.noreply.github.com>
2025-03-31 22:16:46 -0400

    add aliases

--- default.nix
+++ default.nix
@@ -1,13 +1,25 @@
-{stdenv}: stdenv.mkDerivation (finalAttrs: {
-    pname = "cppman-pages";
-    version = "0.0.1";
-    src = ./cache/cppreference.com;
-    installPhase = ''
-        runHook preInstall
+{
+  stdenv,
+  rename,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "cppman-pages";
+  version = "0.0.1";
+  nativeBuildInputs = [
+    rename
+  ];
+  src = ./cache/cppreference.com;
+  installPhase = ''
+    runHook preInstall
 
-        mkdir -p $out/bin
-        mkdir -p $out/share/man/man3
+    mkdir -p $out/bin
+    mkdir -p $out/share/man/man3
 
-        cp ./* $out/share/man/man3
-    '';
+    cp ./* $out/share/man/man3
+    # only link instead of hard rename
+    # Lib keywords
+    rename -l 's/C\+\+ keyword: (\w+)\.(.*?)$/cpp_kw_$1.$2/' $out/share/man/man3/*
+    # headers
+    renqme -l 's/(?:Standard|Experimental) library header (<\w+>)\.(.*?)$/$1.$2/' $out/share/man/man3/*
+  '';
 })
--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
                 html5lib
               ]
             ))
+            rename
           ];
         };
         packages = {
